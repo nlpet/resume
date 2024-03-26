f2b3a2c3da01a4abe4f71fe0f327414bd286c2a2
Nora Petrova <nora@planting.space>
2024-03-26 17:18:05 +0000

    Initial commit: up-to-date CV

---
+++ src/metadata/metadata.js
@@ -0,0 +1,224 @@
+module.exports = {
+  name: "Nora Petrova",
+  title: "Machine Learning Engineer",
+  facts: {
+    Location:
+      '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
+    GitHub: `<a href="https://github.com/nlpet">
+                  <i class="fa-brands fa-github fact-icon"></i>github.com/nlpet
+                </a>`,
+    LinkedIn: `<a href="https://linkedin.com/in/nora-petrova">
+              <i class="fa-brands fa-linkedin fact-icon"></i>linkedin.com/in/nora-petrova
+            </a>`,
+    Email:
+      '<a href="mailto:nora.axion@gmail.com"><i class="fa fa-envelope fact-icon"></i>nora.axion@gmail.com</a>',
+  },
+  positions: [
+    {
+      title: "AI Engineer / Consultant @ Prolific [contract]",
+      period: "May 2023 - Ongoing",
+      skills: [
+        "NLP",
+        "LLMs",
+        "RLHF",
+        "Python",
+        "Javascript",
+        "Next.js",
+        "R&D",
+        "AWS",
+        "Product",
+        "Full Stack",
+      ],
+      contents: `- Advising Prolific on AI strategy and product development, primarily within NLP
+- Researched, developed and open sourced a [social reasoning dataset](https://huggingface.co/datasets/ProlificAI/social-reasoning-rlhf), for fine-tuning of LLMs using RLHF
+- Researching state-of-the-art methods for aligning LLMs to human values, e.g. democratic-fine-tuning
+`,
+    },
+    {
+      title: "NLP Engineer / Julia Developer @ planting.space [contract]",
+      period: "May 2023 - Mar 2024",
+      skills: [
+        "NLP",
+        "LLMs",
+        "Python",
+        "Julia",
+        "Javascript",
+        "React",
+        "AWS",
+        "Research",
+        "Full Stack",
+      ],
+      contents: `- Developed a knowledge representation system by leveraging insights from Bayesian Inference, Category
+      Theory and Natural Language Processing, using the Julia programming language and Python for NLP
+- Researched and implemented state of the art methods within NLP using LLMs, including advanced prompting
+techniques and fine-tuning of models
+- Developed, deployed and maintained an internal search tool for finding relevant internal documents using
+semantic search on chunked vectorised documents. The tool integrated with multiple internal systems
+`,
+    },
+    {
+      title: "Lead Engineer @ Signal.ai [full-time]",
+      period: "Apr 2022 - May 2023",
+      skills: [
+        "NLP",
+        "Python",
+        "Clojure",
+        "R&D",
+        "Modelling",
+        "Terraform",
+        "MLOps",
+        "AWS",
+        "Full-Stack",
+        "Management",
+        "Tech Lead",
+      ],
+      contents: `- Led a team of engineers and researchers with the aim of developing machine learning models and services
+      for MD (mention detection), ED (entity disambiguation), detecting sentiment, text classification, zero/ few shot
+      learning approaches and custom models relevant to our clients
+- Involved in researching relevant state-of-the-art techniques within NLP, prototyping applications, developing
+production ready pipeline services and optimising models for production
+- Involved in bringing state-of-the-art approaches within NLP to client projects (transformer based models)
+- Responsibilities include maintenance of existing pipeline services, roadmap for the team, strategy from a
+product point of view and continuous improvement of services
+      `,
+    },
+    {
+      title: "Senior Product Engineer @ Primer.ai [full-time]",
+      period: "May 2021 - Mar 2022",
+      skills: ["NLP", "Python", "Full Stack", "R&D", "Product"],
+      contents: `- Involved in building NLP powered applications in a full stack capacity (React, Typescript, Python)
+- Responsible for performing data analysis, modelling and preparation, and coming up with machine learning
+approaches (within NLP) to client problems
+- Involved in bringing state-of-the-art approaches within NLP to client projects (transformer based models)
+- Researched appropriate storage layers for knowledge bases, as well as techniques for building them (Neo4J, MongoDB, Postgres)
+      `,
+    },
+    {
+      title: "Principal Machine Learning Engineer @ Datatonic [full-time]",
+      period: "Nov 2019 - May 2021",
+      skills: [
+        "ML",
+        "Python",
+        "Full Stack",
+        "R&D",
+        "GCP",
+        "MLOps",
+        "Modelling",
+        "Productionisation",
+        "Management",
+        "Client Projects",
+      ],
+      contents: `- Led multiple client projects from a technical standpoint; providing direction for the technical delivery and
+      architecting machine learning based solutions on Google Cloud Platform
+- Performed research and applied state-of-the-art machine learning models and techniques in client projects
+- Mentored more junior members of the team and managed a small team
+- Participated in workshops and presented at client facing events
+- Completed Google’s Professional Machine Learning Engineer and Cloud Architect certifications
+      `,
+    },
+    {
+      title: "Senior Machine Learning Engineer @ Datatonic [full-time]",
+      period: "Sept 2018 - Nov 2019",
+      skills: [
+        "ML",
+        "Python",
+        "Javascript",
+        "React",
+        "Full Stack",
+        "R&D",
+        "GCP",
+        "MLOps",
+        "Modelling",
+        "Productionisation",
+        "Client Projects",
+      ],
+      contents: `- Worked on client projects to build and deploy machine learning models on Google Cloud Platform. This
+      usually involved performing data analysis, building data processing pipelines, researching and developing
+      machine learning models, and delivering solutions in short timeframes
+- Worked on a variety of proof-of-concept projects for clients in problem areas such as NLP, document
+classification, computer vision, anomaly detection, recommender systems, and forecasting. The technical stack
+consisted of Python, Tensorflow / Keras / scikit-learn on GCP
+- Led an internal project for standardising machine learning approaches and defining best practices
+- Completed Google’s Professional Data Engineer and Cloud Architect certifications
+      `,
+    },
+    {
+      title:
+        "Machine Learning Engineer / Researcher & Full Stack Software Engineer @ Prodo.ai [full-time]",
+      period: "June 2017 - Sept 2018",
+      skills: [
+        "ML",
+        "Python",
+        "Typescript",
+        "React",
+        "Full Stack",
+        "R&D",
+        "AWS",
+      ],
+      contents: `- Worked on developing deep learning models by leveraging the latest research in AI within graph based
+      neural networks. Primarily using Python, PyTorch and NumPy for model building; pandas, scikit-learn,
+      matplotlib for data, evaluation and visualisation.
+- Worked across the stack on building an application which communicates machine learning insights to clients
+using Javascript, HTML/CSS, NodeJS, Koa, React.js, Redux, Webpack, Ava, RabbitMQ, Kubernetes, MySQL,
+Elasticsearch, docker, AWS, Google Cloud and others.
+- Developed infrastructure tools for running machine learning experiments using primarily Python and AWS.
+      `,
+    },
+    {
+      title: "Full Stack Software Engineer @ R3PI [full-time]",
+      period: "Feb 2017 - June 2018",
+      skills: ["Javascript", "NodeJS", "React", "Docker", "MongoDB", "TDD"],
+      contents: `- Built a suite of micro services which provide metrics and analytics, enabling clients to make better informed
+      decisions about the vehicles under their control. The technologies we used were: Javascript, NodeJS, MySQL,
+      MongoDB, Docker, Mocha, Chai, Istanbul, Bluebird, Hapi, Joi, Bunyan, Bookshelf, Knex, Mongoose among others.
+- Project was completed in May and was deployed and used by a major client
+      `,
+    },
+    {
+      title: "Full Stack Software Engineer @ Digital Catapult [full-time]",
+      period: "Nov 2015 - July 2016",
+      skills: [
+        "Javascript",
+        "NodeJS",
+        "React",
+        "Python",
+        "AWS",
+        "Terraform",
+        "Hadoop",
+        "MySQL",
+        "SPARQL",
+      ],
+      contents: `- Delivered an open source project which is an implementation of an open online digital rights service. The
+      back-end was written in Python using the Tornado async framework. We also used Django, Flask, behave,
+      pytest, Blazegraph (graph db using RDF/SPARQL), MySQL and others. The front-end was written in Javascript
+      with React.js, Bacon.js, gulp and others.
+- Worked on designing, building, testing and deploying the above mentioned project and helped on building an
+openstack cluster for a project that enables data scientists to mix closed data sets in a secure environment.
+Technologies used: Openstack, Hadoop, Ambari, Terraform, vagrant, docker, chef and AWS.
+      `,
+    },
+    {
+      title: "Full Stack Software Engineer @ AWS.com [full-time]",
+      period: "Dec 2012 - Nov 2015",
+      skills: ["Javascript", "React", "Python", "MySQL", "Full Stack"],
+      contents: `- Worked on building a ticketing application which was to replace the existing system and make it easier to
+      maintain, extend, integrate analytics and modernise the existing experience for our customers. The front-end
+      was built using Javascript and React.js, and the back-end was written in Python. I worked across the stack.
+- Implemented core functionality and helped with the roll-out of the product
+      `,
+    },
+  ],
+  experience: [
+    {
+      header: "Testing LLMs for Autonomous Capabilities Hackathon (Mar 2024)",
+      skills: ["NLP", "LLMs", "R&D"],
+      contents: `Participated in a [Hackathon](https://www.apartresearch.com/event/codered) organised by [Apart](https://www.apartresearch.com/) and [METR](https://metr.org/) where the goal was to develop tasks to detect replication and R&D capabilities of LLMs. I worked on a task about capabilities of LLMs to introduce backdoors in datasets and to fine-tune other models on them. Based on research from the [Sleeper Agents paper](https://arxiv.org/abs/2401.05566) by Anthorpic.
+      `,
+    },
+    {
+      header: "Anthropic Claude Hackathon (Nov 2023)",
+      skills: ["NLP", "LLMs", "Next.js", "React", "Javascript", "R&D"],
+      contents: `Participated in a [Hackathon](https://anthropiclondon.devpost.com/) organised by Anthropic London. I developed an app that uses AI (Clade) to help us make sense of the News. Its aim is to introduce nuance and multiple perspectives, and enable us to entertain multiple points of view when we make up our minds about current events. [GitHub Repository](https://github.com/nlpet/anthropic-hackathon-claude) for the project.`,
+    },
+  ],
+};
